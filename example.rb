77f69af4a879b9bd18c665536ed5091fc57def20
Claudio <claudio@cfiorini.it>
2008-12-19 19:26:33 +0100

    just infos

--- example.rb
+++ example.rb
@@ -1,6 +1,9 @@
 require "rjoystick.so"
 
 =begin
+# example 1
+#
+#
 # ARGV[0] => /dev/input/jsX
 k = Rjoystick::Device.new(ARGV[0])
 puts k.axes
@@ -8,10 +11,11 @@ puts k.buttons
 puts k.name
 puts k.version
 puts k.axes_maps
-while true
+while true 
 	e = k.event
-#	if e.type == Rjoystick::Event::JSBUTTON
-#		puts "Button: #{e.number} | #{e.type} #{e.value} #{e.time}"
+	if e.type == Rjoystick::Event::JSBUTTON
+		break if e.number == 0
+		puts "Button: #{e.number} | #{e.type} #{e.value} #{e.time}"
 	if e.type == Rjoystick::Event::JSAXIS
 		puts "#{e.number} #{e.type} #{e.value} #{e.time}"
 	end	
@@ -19,7 +23,30 @@ end
 k.close
 =end
 
+
+# example 2
+#
+#
+# to get this example works check README first.
 # ARGV[0] => /dev/hidrawX
+n = 0
 k = Rjoystick::SixAxis.new(ARGV[0])
-	puts k.get_sixaxis.inspect
+	x = -1
+	y = -1
+	z = -1
+	offset = 40
+	while true
+		break if n == 100
+		l = k.get_sixaxis
+		ax = (x - l[:x]).abs
+		ay = (y - l[:y]).abs
+		az = (z - l[:z]).abs
+		if ax > offset || ay > offset || az > offset
+			puts "#{l[:x]} #{l[:y]} #{l[:z]}" 
+			x = l[:x]
+			y = l[:y]
+			z = l[:z]
+			n += 1
+		end
+	end
 k.close
